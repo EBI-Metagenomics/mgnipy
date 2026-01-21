81f0cc50de1f8f99c4df9e6a83a67adab52f0db2
angelphanth <angel.phanth@hotmail.com>
2026-01-21 16:36:19 +0000

    :heavy-plus-sign: added the auto generated client libraries as dep

--- docs/conf.py
+++ docs/conf.py
@@ -130,7 +130,7 @@ if os.environ.get("READTHEDOCS") == "True":
     from pathlib import Path
 
     PROJECT_ROOT = Path(__file__).parent.parent
-    PACKAGE_ROOT = PROJECT_ROOT / "src" / "mgnipy"
+    PACKAGE_ROOT = PROJECT_ROOT / "mgnipy"
 
     def run_apidoc(_):
         from sphinx.ext import apidoc
