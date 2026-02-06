b5df299bd16494897be4a4aca76cb44dc5f79ef1
angelphanth <angel.phanth@hotmail.com>
2026-02-06 15:21:56 +0000

    semaphore

--- mgnipy/_internal_functions/__init__.py
+++ mgnipy/_internal_functions/__init__.py
@@ -1,16 +1,15 @@
+from .async_helpers import (
+    get_semaphore,
+)
 from .utils import (
     config_loader,
     create_folder,
     get_args,
 )
 
-from .async_helpers import (
-    get_semaphore,
-)
-
 __all__ = [
-    "config_loader", 
-    "create_folder", 
+    "config_loader",
+    "create_folder",
     "get_args",
     "get_semaphore",
 ]
