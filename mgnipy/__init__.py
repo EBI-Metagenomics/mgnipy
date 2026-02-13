60fabcfaf3ce0645f5dd56ff1da715fa9aa5b078
angelphanth <angel.phanth@hotmail.com>
2026-02-13 16:31:56 +0000

    :heavy_plus_sign: cache dependencies

--- mgnipy/__init__.py
+++ mgnipy/__init__.py
@@ -1,3 +1,10 @@
 from importlib import metadata
+from platformdirs import user_cache_dir
 
-__version__ = metadata.version("mgnipy")
+APPNAME = "mgnipy"
+APPAUTHOR = "MGnify"
+CACHE_DIR = user_cache_dir(APPNAME, APPAUTHOR)
+
+__version__ = metadata.version(APPNAME)
+
+# TODO: at init cache the list of biomes
