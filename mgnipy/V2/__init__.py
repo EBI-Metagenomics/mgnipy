d0f470e8986fdc50d40ab7e3d65c084dcc3951c8
angelphanth <angel.phanth@hotmail.com>
2026-04-01 22:45:37 +0100

    fix capitilization of MGnifier

--- mgnipy/V2/__init__.py
+++ mgnipy/V2/__init__.py
@@ -1,4 +1,4 @@
-from .core import Mgnifier
+from .core import MGnifier
 from .mgni_py_v2.client import (
     AuthenticatedClient,
     Client,
@@ -12,7 +12,7 @@ from .proxies import (
 )
 
 __all__ = (
-    "Mgnifier",
+    "MGnifier",
     "Biomes",
     "Studies",
     "Samples",
