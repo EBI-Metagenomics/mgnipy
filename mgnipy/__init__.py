af147643cb174b4af61b5eb0364b8e648de725a1
angelphanth <angel.phanth@hotmail.com>
2026-04-27 14:17:01 +0100

    cache dir back

--- mgnipy/__init__.py
+++ mgnipy/__init__.py
@@ -1,13 +1,12 @@
 from importlib import metadata
 
+from platformdirs import user_cache_dir
+
 from mgnipy.mgnipy import MGnipy as MGnipy
-
-# from platformdirs import user_cache_dir
-
 
 APPNAME = "mgnipy"
 APPAUTHOR = "MGnify"
-# CACHE_DIR = user_cache_dir(APPNAME, APPAUTHOR)
+CACHE_DIR = user_cache_dir(APPNAME, APPAUTHOR)
 
 __version__ = metadata.version(APPNAME)
 
