871b2ae497cf163f0df6f5bf7d27496ec9a95d72
angelphanth <angel.phanth@hotmail.com>
2026-05-19 13:25:29 +0100

    add MGazine for downloading at Analyses list level via .datasets

--- mgnipy/V2/proxies/analyses.py
+++ mgnipy/V2/proxies/analyses.py
@@ -4,7 +4,7 @@ from typing import (
     Literal,
     Optional,
 )
-
+import pandas as pd
 from mgnipy.V2.proxies import MGnifyDetail, MGnifyList
 
 from mgnipy.V2.datasets import MGazine
@@ -22,6 +22,34 @@ class Analyses(MGnifyList):
     ):
 
         super().__init__(params=params, config=config, **kwargs)
+
+    @property
+    def details_downloads_list(self) -> list[dict[str, Any]]:
+        return [
+            item for sublist in self.details_df["downloads"].values for item in sublist
+        ]
+
+    @property
+    def details_downloads_df(self) -> pd.DataFrame:
+        # return self.details_df["downloads"]
+
+        down_dict = self.details_df["downloads"]
+
+        okie = {x: y for x, y in down_dict.items() if len(y) > 0}
+        rows = [
+            dict(study_id=study, **item)
+            for study, items in okie.items()
+            for item in items
+        ]
+        return pd.DataFrame(rows)
+
+    @property
+    def datasets(self):
+        """A property that returns an MGazine instance containing the downloads information for the study."""
+        return MGazine(
+            downloads=self.details_downloads_list,
+            config=self.config,
+        )
 
 
 class AnalysisDetail(MGnifyDetail):
