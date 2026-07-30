2b610b9e7436f20f685b4c8b09704416766f7123
Angel L. P. <59593766+angelphanth@users.noreply.github.com>
2026-07-30 16:05:20 +0200

    updates to .metadata() to auto combine runs, biosamples, sample metadata and :fire: remove unused code

--- mgnipy/V2/datasets/annotate.py
+++ mgnipy/V2/datasets/annotate.py
@@ -4,74 +4,21 @@ logger = logging.getLogger(__name__)
 
 import pandas as pd
 import polars as pl
-from typing import (
-    Any,
-    Literal,
-)
+from typing import Any, Literal, Optional
 
 from mgnipy.V2.mgnifier.endpoints import ID_PARAM
-from mgnipy.V2.mixins import CheckpointMixin
 from mgnipy._models.constants.CONSTANTS import SupportedEndpoints
 from mgnipy.V2.mgnifier.metadata import MGnifyMetadata, ResultsHandler
-
-_RESULT_PAGE_BY_FIELD = {
-    "mgnify_runs": 1,
-    "mgnify_samples": 2,
-    "mgnify_studies": 3,
-    "biosamples_metadata": 4,
-    "mgnify_analyses": 5,
-    "mgnify_assemblies": 6,
-}
-
-_JOIN_ON = {
-    "mgnify_runs__mgnify_samples": {
-        "left_on": "sample_accession",
-        "right_on": ID_PARAM[SupportedEndpoints.SAMPLES],
-    },
-    "mgnify_runs__mgnify_studies": {
-        "left_on": "study_accession",
-        "right_on": ID_PARAM[SupportedEndpoints.STUDIES],
-    },
-    "mgnify_assemblies__mgnify_runs": {
-        "left_on": "run_accession",
-        "right_on": ID_PARAM[SupportedEndpoints.RUNS],
-    },
-    "mgnify_assemblies__mgnify_samples": {
-        "left_on": "sample_accession",
-        "right_on": ID_PARAM[SupportedEndpoints.SAMPLES],
-    },
-    "mgnify_assemblies__mgnify_studies": {
-        "left_on": "assembly_study_accession",
-        "right_on": ID_PARAM[SupportedEndpoints.STUDIES],
-    },
-    "biosamples_metadata__mgnify_runs": {
-        "left_on": "GivenID",
-        "right_on": "sample_accession",
-    },
-}
 
 
 class MetadataSettersMixin:
 
     def _set_cached_list(self, field: str, value: list[dict[str, Any]]) -> None:
         setattr(self, f"_{field}", value)
-        try:
-            self.write_results(self._RESULT_PAGE_BY_FIELD[field], value)
-            logger.debug(f"Results written for field '{field}'.")
-        except AttributeError as e:
-            logger.debug(
-                f"CheckpointMixin not enabled. Cannot write results for field '{field}'. {e}"
-            )
 
     def _append_cached_item(self, field: str, value: dict[str, Any]) -> None:
         current = getattr(self, f"_{field}")
         current.append(value)
-        try:
-            self.write_results(self._RESULT_PAGE_BY_FIELD[field], current)
-        except AttributeError as e:
-            logger.debug(
-                f"CheckpointMixin not enabled. Cannot write results for field '{field}'. {e}"
-            )
 
     @property
     def mgnify_studies(self) -> MGnifyMetadata:
@@ -143,69 +90,151 @@ class MetadataSettersMixin:
         self,
         df_engine: Literal["polars", "pandas"] = "pandas",
         expand_nested_dicts: bool = True,
-        how="full",
+        how="left",
         coalesce: bool = True,
+        for_runs: Optional[list[str]] = None,
     ) -> pl.DataFrame | pd.DataFrame:
 
-        # init dict
-        paired_dfs: dict[str, pl.DataFrame] = {}
-        # go through each pair in _JOIN_ON and perform the join
-        for pair in _JOIN_ON:
-            left, right = pair.split("__")
-            left_data: MGnifyMetadata = getattr(self, left)
-            if len(left_data) == 0:
-                logger.warning(
-                    f"Dataset '{left}' is empty. Skipping join for this pair."
-                )
-                continue
-            right_data: MGnifyMetadata = getattr(self, right)
-            if len(right_data) == 0:
-                logger.warning(
-                    f"Dataset '{right}' is empty. Skipping join for this pair."
-                )
-                continue
+        _runs = for_runs or getattr(self, "runs_accessions", None)
+        if _runs is None:
+            logger.warning("No runs accessions provided. Returning empty dataframe.")
+            return pl.DataFrame() if df_engine == "polars" else pd.DataFrame()
 
-            left_df: pl.DataFrame = left_data.to_polars(
+        # getting run accessions as sorted_index
+        sorted_index = sorted(self.X(df_engine="polars").columns)
+
+        base = pl.DataFrame(sorted_index, schema=["mgnify_run_accession_index"])
+
+        if len(self.available_metadata_sets) == 0:
+            logger.warning(
+                "No non-empty metadata sets available. Returning empty dataframe."
+            )
+
+            if df_engine == "polars":
+                logger.warning("Polars dataframe does not support empty df with index.")
+                return base
+
+            if df_engine == "pandas":
+                return pd.DataFrame(index=sorted_index)
+
+        # for runs
+        if (
+            "mgnify_runs" not in self.available_metadata_sets
+            and "biosamples_metadata" not in self.available_metadata_sets
+        ):
+            logger.warning(
+                "runs metadata set is not available. Returning empty dataframe."
+            )
+            return (
+                pl.DataFrame(sorted_index)
+                if df_engine == "polars"
+                else pd.DataFrame(index=sorted_index)
+            )
+
+        elif (
+            "mgnify_runs" in self.available_metadata_sets
+            and "biosamples_metadata" not in self.available_metadata_sets
+        ):
+            pl_runs = self.mgnify_runs.to_polars(
                 expand_nested_dicts=expand_nested_dicts
             )
-            right_df: pl.DataFrame = right_data.to_polars(
+
+            base = base.join(
+                pl_runs,
+                how=how,
+                coalesce=coalesce,
+                left_on="mgnify_run_accession_index",
+                right_on=ID_PARAM[SupportedEndpoints.RUNS],
+            )
+        elif (
+            "biosamples_metadata" in self.available_metadata_sets
+            and "mgnify_runs" not in self.available_metadata_sets
+        ):
+            pl_biosamples = self.biosamples_metadata.to_polars(
                 expand_nested_dicts=expand_nested_dicts
             )
-            join_params: dict[str, str] = _JOIN_ON[pair]
-            merged: pl.DataFrame = left_df.join(
-                right_df, how=how, coalesce=coalesce, **join_params
+
+            base = base.join(
+                pl_biosamples,
+                how=how,
+                coalesce=coalesce,
+                left_on="mgnify_run_accession_index",
+                right_on="RunID",
             )
-            paired_dfs[pair] = merged
+        elif (
+            "biosamples_metadata" in self.available_metadata_sets
+            and "mgnify_runs" in self.available_metadata_sets
+        ):
+            pl_biosamples = self.biosamples_metadata.to_polars(
+                expand_nested_dicts=expand_nested_dicts
+            )
+            pl_runs = self.mgnify_runs.to_polars(
+                expand_nested_dicts=expand_nested_dicts
+            )
 
-        return paired_dfs
+            base = base.join(
+                pl_runs,
+                how=how,
+                coalesce=coalesce,
+                left_on="mgnify_run_accession_index",
+                right_on=ID_PARAM[SupportedEndpoints.RUNS],
+            )
 
+            base = base.join(
+                pl_biosamples,
+                how=how,
+                coalesce=coalesce,
+                left_on="mgnify_run_accession_index",
+                right_on="RunID",
+            )
 
-class MetadataCheckpointMixin(CheckpointMixin):
+        # now to the samples
+        if (
+            "mgnify_samples" in self.available_metadata_sets
+            and "mgnify_runs" in self.available_metadata_sets
+        ):
 
-    @property
-    def resource(self) -> SupportedEndpoints:
-        """for checkpointmixin"""
-        return getattr(self, "_resource", None) or SupportedEndpoints(
-            "_custom_endpoint"
+            right = self.mgnify_samples.to_polars(
+                expand_nested_dicts=expand_nested_dicts
+            )
+
+            base = base.join(
+                right,
+                how=how,
+                coalesce=coalesce,
+                left_on="sample_accession",
+                right_on=ID_PARAM[SupportedEndpoints.SAMPLES],
+            )
+            return (
+                base
+                if df_engine == "polars"
+                else base.to_pandas().set_index("mgnify_run_accession_index")
+            )
+
+        if (
+            "mgnify_samples" in self.available_metadata_sets
+            and "biosamples_metadata" in self.available_metadata_sets
+        ):
+
+            right = self.biosamples_metadata.to_polars(
+                expand_nested_dicts=expand_nested_dicts
+            )
+
+            base = base.join(
+                right,
+                how=how,
+                coalesce=coalesce,
+                left_on="sample_accession",
+                right_on="SampleID",
+            )
+            return (
+                base
+                if df_engine == "polars"
+                else base.to_pandas().set_index("mgnify_run_accession_index")
+            )
+
+        return (
+            base
+            if df_engine == "polars"
+            else base.to_pandas().set_index("mgnify_run_accession_index")
         )
-
-    @property
-    def params(self) -> dict[str, Any]:
-        if getattr(self, "_params", None) is not None:
-            return self._params
-        else:
-            return {
-                "mgazine": str(self),
-                "short_desc": self.short_desc,
-                "resource": self.resource.value,
-            }
-
-    def load_cache(self):
-        self._results = None
-        page_nums = self.load_cache_results()
-        if self._results:
-            for each in _RESULT_PAGE_BY_FIELD:
-                setattr(
-                    self, f"_{each}", self._results.get(_RESULT_PAGE_BY_FIELD[each], [])
-                )
-        return page_nums
